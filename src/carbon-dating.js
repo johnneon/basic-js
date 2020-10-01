44a9297fda1c9b36a2781a166b8c292de9707600
johnneon <neon.1598@mail.ru>
2020-10-01 13:59:12 +0600

    init: start basik-js task + feat: add 3 functions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,11 @@
-const CustomError = require("../extensions/custom-error");
-
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (typeof sampleActivity !== 'string' || +sampleActivity > 15 
+  || +sampleActivity <= 0 || isNaN(parseFloat(sampleActivity)) || !sampleActivity) {
+    return false;
+  }
+  sampleActivity = parseFloat(sampleActivity);
+  return Math.ceil((Math.log2(MODERN_ACTIVITY) - Math.log2(sampleActivity)) * HALF_LIFE_PERIOD * 1.000214);
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,10 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let count = 0,
+      arr = matrix.flat(Infinity);
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == "^^") {
+          count++;
+        }
+      }
+  return count;
 };
