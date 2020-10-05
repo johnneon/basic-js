29221c2ae8fd6e85835c3f629a7c6140a1d7b3a5
johnneon <neon.1598@mail.ru>
2020-10-05 15:21:39 +0600

    feat: add transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+  if (arr.length === 0) return arr;
+  let result = arr.slice();
+  for (let i = 0; i < result.length; i++) {
+      if (typeof result[i] === 'string') {
+          if (result[i] === '--discard-next') {
+            delete result[i];
+            delete result[i + 1];
+          }
+          else if (result[i] === '--discard-prev') {
+            delete result[i];
+            delete result[i - 1];
+          }
+          else if (result[i] === '--double-next') {
+              delete result[i];
+              result.splice(i + 1, 0, result[i + 1]);
+          } else if (result[i] === '--double-prev') {
+              delete result[i];
+              result.splice(i, 0, result[i - 1]);
+              i++;
+          }
+      }
+  }
+  return result.filter(el => el !== undefined);
 };
