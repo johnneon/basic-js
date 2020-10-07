666d13d4fc23771d572c20c5d7a72c7ffd84bdb9
johnneon <neon.1598@mail.ru>
2020-10-08 00:24:04 +0600

    feat: add vegenere cipher class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,77 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse) {
+    if (reverse === false) this.reverse = false;
+    else this.reverse = true;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.len = this.alphabet.length;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error();
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+    let keyArray = [];
+    let shift = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      let order = this.alphabet.indexOf(message[i]);
+      let keyNumber = null;
+      if (order === -1) {
+        keyNumber = message[i];
+        shift++;
+      } else {
+        keyNumber = key[(i - shift) % key.length];
+      }
+      keyArray.push(keyNumber);
+    }
+
+    for (let j = 0; j < message.length; j++) {
+      let num = this.alphabet.indexOf(message[j]);
+      let letter;
+      if (num === -1) letter = message[j]
+      else {
+        letter = this.alphabet[(this.alphabet.indexOf(message[j]) + this.alphabet.indexOf(keyArray[j])) % this.len];
+      }
+      result += letter;
+    }
+
+    if (!this.reverse) return result.split('').reverse().join('');
+    return result;
+  }
+  
+  decrypt(message, key) {
+    if (!message || !key) throw new Error();
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+    let keyArray = [];
+    let shift = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      let order = this.alphabet.indexOf(message[i]);
+      let keyNumber = null;
+      if (order === -1) {
+        keyNumber = message[i];
+        shift++;
+      } else keyNumber = key[(i - shift) % key.length];
+      keyArray.push(keyNumber);
+    }
+
+    for (let j = 0; j < message.length; j++) {
+      let num = this.alphabet.indexOf(message[j]);
+      let letter;
+      if (num === -1) {
+        letter = message[j];
+      }
+      else {
+        letter = this.alphabet[(this.alphabet.indexOf(message[j]) - this.alphabet.indexOf(keyArray[j]) + this.len) % this.len];
+      }
+      result += letter;
+    }
+
+    if (!this.reverse) return result.split('').reverse().join('');
+    return result;
   }
 }
-
 module.exports = VigenereCipheringMachine;
